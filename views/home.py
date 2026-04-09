cf735b33694e40afb9d1133d0aa984ad66e8a7fa
steffinitely <bierist3@students.zhaw.ch>
2026-04-09 18:56:06 +0200

    Wort änderungen

--- views/home.py
+++ views/home.py
@@ -13,10 +13,10 @@ st.markdown("""Progressive Overload ist eine Streamlit-App, die dir hilft, dein 
 Anstatt Trainingsgewichte nach Gefühl zu wählen, basiert unsere App auf wissenschaftlichen Prinzipien des Muskelaufbaus und liefert dir für jede Trainingseinheit das optimale Gewicht. 
 
 Die Idee dahinter ist einfach: Fortschritt entsteht nur, wenn dein Körper kontinuierlich neuen Reizen ausgesetzt wird. Genau hier setzt Progressive Overload an.
-Die App analysiert deine bisherigen Trainingsdaten und berechnet daraus präzise, wie stark du das Gewicht steigern solltest, um maximale Fortschritte zu erzielen.
+Die App analysiert deine bisherigen Trainingsdaten und berechnet daraus präzise, wie stark du das Trainingsvolumen steigern solltest, um maximale Fortschritte zu erzielen.
 
 Egal ob Anfänger oder Fortgeschrittener: Du bekommst klare Empfehlungen, die dir helfen, effizienter zu trainieren, Plateaus zu vermeiden und langfristig stärker zu werden.
 Statt Rätselraten erhältst du datenbasierte Entscheidungen 
 
-Mit Progressive Overload trainierst du nicht härter, sondern smarter.
+Mit Progressive Overload trainierst du nicht nur härter, sondern auch smarter.
 """)
