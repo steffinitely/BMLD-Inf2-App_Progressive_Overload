1fb585875b66c677165bddb59aecddf7995bd839
Leah Talary <talarlea@students.zhaw.ch>
2026-04-15 08:19:00 +0200

    Unterseite umbenennen

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ import streamlit as st
 st.set_page_config(page_title="Meine App", page_icon=":material/home:")
 
 pg_home = st.Page("views/home.py", title="Home", icon=":material/home:", default=True)
-pg_second = st.Page("views/unterseite_a.py", title="Unterseite A", icon=":material/info:")
+pg_login= st.Page("views/login.py", title="Login", icon=":material/info:")
 
-pg = st.navigation([pg_home, pg_second])
+pg = st.navigation([pg_home, pg_login])
 pg.run()
 
 # app.py
---
+++ views/login.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("Login")
+
+st.write("Bitte loggen Sie sich ein, um fortzufahren.")
