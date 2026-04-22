3fe5363b666febafa1ae85a777a9504e73f41b9b
mburns1234 <brandmer@students.zhaw.ch>
2026-04-22 11:07:08 +0200

    Login unterseite und sign up unterseite gelöscht

--- app.py
+++ app.py
@@ -3,8 +3,6 @@ import streamlit as st
 st.set_page_config(page_title="Meine App", page_icon=":material/home:")
 
 pg_homepage = st.Page("views/homepage.py", title="Home", icon=":material/home:", default=True)
-pg_login= st.Page("views/login.py", title="Login", icon=":material/info:")
-pg_signup = st.Page("views/signup.py", title="Sign Up", icon=":material/info:")
 pg_workout_selection = st.Page("views/workout_selection.py", title="Workout Auswahl", icon=":material/info:")
 pg_upper_exercise_selection = st.Page("views/upper_exercise_selection.py", title="Oberkörper Übungen", icon=":material/info:")
 pg_lower_exercise_selection = st.Page("views/lower_exercise_selection.py", title="Unterkörper Übungen", icon=":material/info:")
@@ -17,7 +15,7 @@ pg_lower_e3 = st.Page("views/lower_e3.py", title="Unterkörper Übung 3", icon="
 
 
 
-pg = st.navigation([pg_homepage, pg_login, pg_signup, pg_workout_selection, 
+pg = st.navigation([pg_homepage, pg_workout_selection, 
                     pg_upper_exercise_selection, pg_lower_exercise_selection, 
                     pg_upper_e1, pg_upper_e2, pg_upper_e3, pg_lower_e1, 
                     pg_lower_e2, pg_lower_e3])
--- views/login.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-st.title("Login")
-
-st.write("Bitte loggen Sie sich ein, um fortzufahren.")
