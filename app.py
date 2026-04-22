b49358fac4fa84a842669ac8e4449ec6b635f427
Leah Talary <talarlea@students.zhaw.ch>
2026-04-22 11:33:30 +0200

    switch drive verknüpfung

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
 import streamlit as st
+import pandas as pd
+from utils.data_manager import DataManager
+from utils.login_manager import LoginManager
 
 st.set_page_config(page_title="Meine App", page_icon=":material/home:")
+
+data_manager = DataManager(fs_protocol='webdav', fs_root_folder='overload')
+login_manager = LoginManager(data_manager)
+login_manager.login_register()
+
+if 'data_df' not in st.session_state:
+    st.session_state['data_df'] = data_manager.load_user_data(
+        'data.csv',
+        initial_value=pd.DataFrame(),
+        parse_dates=['timestamp'])
+
+def load_user_data(self, file_name, initial_value=None, **load_args):
+
+def save_app_data(self, data, file_name):
+
 
 pg_homepage = st.Page("views/homepage.py", title="Home", icon=":material/home:", default=True)
 pg_workout_selection = st.Page("views/workout_selection.py", title="Workout Auswahl", icon=":material/info:")
