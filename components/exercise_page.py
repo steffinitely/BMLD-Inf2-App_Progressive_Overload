efff10cf9e5f717e201449e38f345bd3b65af87c
Leah Talary <talarlea@students.zhaw.ch>
2026-05-06 08:25:54 +0200

    power

--- components/exercise_page.py
+++ components/exercise_page.py
@@ -51,9 +51,11 @@ class ExercisePage:
             st.error("⚠️ Deload empfohlen")
 
         # Zusatzinfo (1RM)
-        st.subheader("Challenge: Probier mal Powerlifting-Style mit 1RM!")
         if est_1rm:
-            st.caption(f"📈 Geschätztes 1RM: {round(est_1rm, 1)} kg")
+            st.caption(
+                title="Challenge: Probier mal Powerlifting mit 1RM!",
+                body=f"📈 Geschätztes 1RM: {round(est_1rm, 1)} kg"
+            )
 
         st.divider()
 
